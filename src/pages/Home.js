2852dd1c432ff47b4969373e9e9b1baab57c0792
LesegoLSG <217057620@student.uj.ac.za>
2023-08-16 13:32:33 -0500

    Added a task for testing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,40 @@ import '../stylesheet/Home.css'
 import { useNavigate } from 'react-router-dom';
 //import Editscreen from './Editscreen';
 //import '../stylesheet/Arrow-left.css'
+import Tasks from '../components/Tasks'
+import {useState} from 'react';
+
+
+
+
 
 const Home = () => {
     const navigate = useNavigate();
+    /*Task component state*/
+    const [stateTask, setStateTask] = useState();
+    /*Temopary database like*/
+const tasks=[
+    {
+        id:1,
+        text:'Doctors Appointment',
+        day:'Feb 5th at 2:30pm',
+        reminder:true,
+    },
+
+    {
+        id:2,
+        text:'Meeting at school',
+        day:'Feb 6th at 2:30pm',
+        reminder:true,
+    },
+
+    {
+        id:3,
+        text:'Food Shopping',
+        day:'Feb 7th at 2:30pm',
+        reminder:false,
+    },
+]
 
     function handleClick(){
         navigate('/Addscreen')
@@ -14,6 +45,7 @@ const Home = () => {
     return (
         <div className="content">
             <button onClick={handleClick}>Add Member</button>
+        <Tasks tasks={tasks}/>
         </div>
     );
 }
