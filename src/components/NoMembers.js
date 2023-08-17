cf1aad46622117e97843f4832df314b46c0b4575
LesegoLSG <217057620@student.uj.ac.za>
2023-08-17 00:33:09 -0500

    final project before using JSON server

---
+++ src/components/NoMembers.js
@@ -0,0 +1,11 @@
+import '../stylesheet/Task.css'
+
+const NoMembers = () => {
+    return (  
+        <div className="no-member-container">
+            <h1>No Member in the database</h1>
+        </div>
+    );
+}
+ 
+export default NoMembers;
