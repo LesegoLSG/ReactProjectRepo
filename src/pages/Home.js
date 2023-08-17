9879b4fe15c74821a169b585285c99ec8bed796a
LesegoLSG <217057620@student.uj.ac.za>
2023-08-17 02:19:49 -0500

    Added a temporary database json

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,30 +14,7 @@ import { useState } from 'react';
 const Home = () => {
     const navigate = useNavigate();
     /*Task component state*/
-    const [tasks, setTasks] = useState(
-        [
-            {
-                id: 1,
-                text: 'Doctors Appointment',
-                day: 'Feb 5th at 2:30pm',
-                reminder: true,
-            },
-
-            {
-                id: 2,
-                text: 'Meeting at school',
-                day: 'Feb 6th at 2:30pm',
-                reminder: true,
-            },
-
-            {
-                id: 3,
-                text: 'Food Shopping',
-                day: 'Feb 7th at 2:30pm',
-                reminder: false,
-            },
-        ]
-    )
+    const [tasks, setTasks] = useState([])
 
 
     //Redirect to Addscreen page
