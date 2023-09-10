5ed398b6bf74acdc9eb0a20e93333b2fe019ff97
LesegoLSG <217057620@student.uj.ac.za>
2023-09-09 21:53:08 -0500

    Created new state for edit

--- src/components/EditTask.js
+++ src/components/EditTask.js
@@ -1,66 +1,47 @@
 import React from 'react';
 import {useState, useEffect,useParams} from 'react';
 import Button from './Button';
-import { BsArrowLeft } from 'react-icons/bs'
+import { BsArrowLeft } from 'react-icons/bs';
+import '../stylesheet/Screenadd.css';
+import Imageicon from '../Icons/Imageicon.png';
 
 
-
-const EditTask = ({taskId,onUpdate}) => {
+const EditTask = ({selectedTask,updateOurTask}) => {
     //states declaration
+    
+    const backpicture = Imageicon;
+    const [urlfile, setUrlfile] = useState(selectedTask.urlImage);
+
     const [firstname, setFirstname] = useState('');
     const [jobtitle, setJobtitle] = useState('');
     const [image, setImage] = useState('');
-    const [urlfile, setUrlfile] = useState('');
-   
-    
-    
-    //Function: Fetches the task/member based on the id
-    const fetchTask = async(id) =>{
-        try{
-            const res = await fetch(`http://localhost:5001/tasks/${id}`);
-            const data = await res.json();
-            return data
 
-        }catch(error){
-            console.error('Could not fetch data from database');
-            throw error;
-        }
-        
-    };
-    //Set image when page loads
-    useEffect(() =>{
-        if(image){
-            setUrlfile(image);
-        }
-    },[image]);
-    //Sets task/member when a page loads
-    useEffect(() =>{
-        fetchTask(taskId).then(myInfo => {
-            setFirstname(myInfo.firstname);
-            setJobtitle(myInfo.jobtitle);
-            setImage(myInfo.urlImage);
+    const [newEditState, setNewEditState] = useState({
+        id:selectedTask.id,
+        firstname:selectedTask.firstname,
+        jobtitle:selectedTask.jobtitle,
+        urlImage:selectedTask.urlImage,
+    });
+
+    const handleChange = (e) =>{
+        setNewEditState({
+            ...newEditState,
+            [e.target.name] : e.target.value
         })
-        .catch(error =>{
-            console.error('Task could not be fetched',error);
-        });
-    },[taskId]);
-    
-    
+    }
+   
+  
+   
 
    
     //Function: handles edit when the button edit is clicked
     const handleSubmit = async (e) =>{
         e.preventDefault();
-        const taskToUpdate ={
-            id: taskId,
-            firstname,
-            jobtitle,
-            urlImage:urlfile,
-        }
-
-        onUpdate(taskToUpdate);
-
+        console.log("submit--task:", newEditState);
+        updateOurTask(newEditState);
     };
+
+    
     //Function: handles image
     const handleImage = (e) =>{
         const file = e.target.files[0];
@@ -69,45 +50,50 @@ const EditTask = ({taskId,onUpdate}) => {
             reader.onload = (e) =>{
                 const dataUrl = e.target.result;
                 setUrlfile(dataUrl);
+                // selectedTask.urlImage=dataUrl;
             };
             reader.readAsDataURL(file);
         }
+        console.log("whole editpage task:", selectedTask);
     };
+    
     //Function: for navigating to the home component when back arrow is clicked
     const homeNavigation = () => {
         window.location.href = 'http://localhost:3000/';
     };
-
+    
     //Below is displayed to the used
     return ( 
         <section>
             <BsArrowLeft style={{color: "blue", fontSize: "50"}} onClick={homeNavigation} />
-            <form className='image-container' onSubmit={handleSubmit}>
+            <form className='image-container' >
 
                 
                 <div className="imageicon">
-                    <img src={urlfile} alt="Preview" className='img-display-after'/>
-                    <input type="file" onChange={handleImage} accept="image/*"/>
+                    <img  src={urlfile} alt="Preview" className='img-display-after'/>
+                    <input type="file"  onChange={handleImage} accept="image/*"/>
                 </div>
             
-
+                <div className='input-container'>
                 <input type="text"
                     required
                     placeholder='Full Names'
                     id='name-field'
-                    value={firstname}
-                    onChange={(e) => setFirstname(e.target.value)}
+                    name= "firstname"
+                    value={newEditState.firstname}
+                    onChange={handleChange}
                 />
                 <input
                     type="text"
                     required
                     placeholder='Job Title'
                     id='job-field'
-                    value={jobtitle}
-                    onChange={(e) => setJobtitle(e.target.value)}
+                    name="jobtitle"
+                    value={newEditState.jobtitle}
+                    onChange={handleChange}
                 />
-            <Button  text='Edit Member'/>
-
+            <Button  text='Edit Member' onClick={handleSubmit}/>
+            </div>
 
             </form>
         </section>
